714e22a38fb1c79b207e8742bad2ca23ad37bebd
sas <sas@tradebyte.biz>
2019-04-04 15:32:43 +0200

    reset quiz on click

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,18 @@ class Quiz extends Component {
         this.setState({quiz_position : this.state.quiz_position + 1})
     }
 
+    handleResetClick() {
+        this.setState({quiz_position : 1});
+    }
+
     render () {
         let question = quizData.quiz_questions[this.state.quiz_position - 1];
         const isQuizEnd = this.state.quiz_position -1 === quizData.quiz_questions.length;
 
         return <div>
-            {(isQuizEnd ? <QuizEnd /> :<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={question} />)}
+            {(isQuizEnd 
+                ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
+                :<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={question} />)}
         </div>
     }
 }
