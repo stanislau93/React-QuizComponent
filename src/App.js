d17e68371ad34da13aa28e42e4e60503f9b83d5b
sas <sas@tradebyte.biz>
2019-04-04 13:25:25 +0200

    added dummy Quiz component to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+
+import Quiz from './Quiz'
+
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div />
+      <Quiz />
     )
   }
 }
